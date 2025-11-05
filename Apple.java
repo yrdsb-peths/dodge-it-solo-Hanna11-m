a532a06c812fd15f6f42c9dfb71631d7aa995d4f
Hanna11-m <440059605@gapps.yrdsb.ca>
2025-11-05 14:08:36 -0500

    Create Apply

---
+++ Apple.java
@@ -0,0 +1,28 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class Apple extends Actor
+{
+    public void act()
+    {
+        move(-4);
+        
+        if(getX() <= 0) {
+           resetApple(); 
+        }
+        
+        if(isTouching(Hero.class)) {
+            Skull skull = new Skull();
+            getWorld().addObject(skull, 300, 200);
+            getWorld().removeObject(this);
+        }
+    }
+    
+    public void resetApple() {
+        int num = Greenfoot.getRandomNumber(2);
+        if(num == 0) {
+            setLocation(600, 100);
+        } else {
+            setLocation(600, 300);
+        }
+    }
+}
